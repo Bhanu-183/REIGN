1af089239dff4e2ea6231951d54af387fa655a67
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-01 00:17:18 +0530

    Updates

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 700) {
-            $(".navbar").css("background", "black");
+            $(".navbar").css("background", "rgba(0,0,0,0.95)");
         } else {
             $(".navbar").css("background", "rgba(0,0,0,0)");
         }
     });
 });
-
 
 function videoEnded() {
     document.getElementById('video').style.display = "none";
