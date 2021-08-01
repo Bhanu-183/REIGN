b8e519790b6aa8d3a5815dfb5bf1f3f663f87086
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-01 16:19:16 +0530

    Updates

--- newsletter.php
+++ newsletter.php
@@ -3,8 +3,14 @@ if (isset($_POST['submit'])) {
     $to = "bhanukrishnaprasad3333@gmail.com";
     $from = $_POST['email'];
     $subject = "Newsletter Subscription";
-    $message = " Name: " . $name . "\nQuery Topic : " . $subject . " \nQuery:" . "\n";
+    $message = "\nQuery Topic : " . $subject;
     $headers = "From:" . $from;
     "From:" . $to;
     $mail_status =  mail($to, $subject, $message, $headers);
+    if ($mail_status) {
+?>
+        <script>
+            location.replace("./index.html");
+        </script>
+<?php }
 }
