493464d6eba045d420111b19c06aafc0779e41a8
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-01 11:59:08 +0530

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+$(window).scroll(startCounter);
+function startCounter() {
+    if ($(window).scrollTop() > 2200) {
+        $(window).off("scroll", startCounter);
+        $('.downloads').each(function () {
+            var $this = $(this);
+            jQuery({
+                Counter: 0
+            }).animate({
+                Counter: $this.text()
+            }, {
+                duration: 1500,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+        $('.minutes').each(function () {
+            var $this = $(this);
+            jQuery({
+                Counter: 0
+            }).animate({
+                Counter: $this.text()
+            }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+        $('.coffees').each(function () {
+            var $this = $(this);
+            jQuery({
+                Counter: 0
+            }).animate({
+                Counter: $this.text()
+            }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+
+    }
+}
+
 $(document).ready(function () {
 
 
