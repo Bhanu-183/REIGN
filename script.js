dcb73484bbec1be545ac88ee84d6ce69bb6f4e71
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-02 16:20:38 +0530

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+$(".navbar-collapse a").click(function () {
+    var element;
+    element = document.getElementById('navbarSupportedContent');
+    element.className = element.className.replace("show", "");
+});
+
+function navchange(evt, navname) {
+    var i, tablinks;
+    tabcontent = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+    }
+
+    document.getElementById(navname).className += " active";
+    // evt.currentTarget.className += " active";
+}
+
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabs");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+function openProject(evt, tabName) {
+    var i, projectcontent, tablinks;
+    projectcontent = document.getElementsByClassName("project-content");
+    for (i = 0; i < projectcontent.length; i++) {
+        projectcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("project-tabs");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" clicked", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " clicked";
+}
+
+
 $(window).scroll(startCounter);
 function startCounter() {
     if ($(window).scrollTop() > 2200) {
